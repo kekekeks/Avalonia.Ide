cfa40f4e2fd86b8af006022dd4a3ac41b14d64a0
Nikita Tsukanov <keks9n@gmail.com>
2017-06-30 11:40:27 +0300

    Use AvaloniaEdit for preview

--- src/Avalonia.Ide.CompletionEngine/Completion.cs
+++ src/Avalonia.Ide.CompletionEngine/Completion.cs
@@ -9,13 +9,14 @@ namespace Avalonia.Ide.CompletionEngine
         public string DisplayText { get; }
         public string InsertText { get; }
         public string Description { get; }
+        public int? RecommendedCursorOffset { get; }
 
-        public Completion(string displayText, string insertText, string description)
+        public Completion(string displayText, string insertText, string description, int? recommendedCursorOffset = null)
         {
             DisplayText = displayText;
             InsertText = insertText;
             Description = description;
-            
+            RecommendedCursorOffset = recommendedCursorOffset;
         }
 
         public Completion(string insertText) : this(insertText, insertText, insertText)
