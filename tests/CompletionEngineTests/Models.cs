4185c9fa972961a71be43814b11d3b12a994845d
Giuseppe Lippolis <workgroupengineering@outlook.com>
2021-11-02 16:27:58 +0100

    feat: : test to discover the internal classes and it is InternalsVisibleTo

--- tests/CompletionEngineTests/Models.cs
+++ tests/CompletionEngineTests/Models.cs
@@ -1,3 +1,5 @@
+using Avalonia;
+
 namespace CompletionEngineTests.Models
 {
     public class GenericBaseClass<T>
@@ -8,5 +10,33 @@ namespace CompletionEngineTests.Models
     public class EmptyClassDerivedFromGenericClassWithDouble : GenericBaseClass<double>
     {
         
+    }
+
+    public static class AttachedBehavior
+    {
+        public static readonly AttachedProperty<bool> IsEnabledProperty =
+            AvaloniaProperty.RegisterAttached<Avalonia.Controls.UserControl, bool>("IsEnabled",
+                typeof(AttachedBehavior),
+                false);
+
+        public static bool GetIsEnabled(Avalonia.Controls.UserControl userControl) =>
+            userControl.GetValue(IsEnabledProperty);
+
+        public static void SetIsEnabled(Avalonia.Controls.UserControl userControl, bool value) =>
+            userControl.SetValue(IsEnabledProperty, value);
+    }
+
+    internal static class InternalAttachedBehavior
+    {
+        public static readonly AttachedProperty<bool> IsEnabledProperty =
+            AvaloniaProperty.RegisterAttached<Avalonia.Controls.UserControl, bool>("IsEnabled",
+                typeof(InternalAttachedBehavior),
+                false);
+
+        public static bool GetIsEnabled(Avalonia.Controls.UserControl userControl) =>
+            userControl.GetValue(IsEnabledProperty);
+
+        public static void SetIsEnabled(Avalonia.Controls.UserControl userControl, bool value) =>
+            userControl.SetValue(IsEnabledProperty, value);
     }
 }
